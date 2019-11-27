d3d51102b736cd1cd9b93f17e9e6b3c83f905181
Arlo Emerson <arloemerson@gmail.com>
2019-11-27 15:45:04 -0800

    adding some quick simple layer modifier scripts

---
+++ after_effects/__shift_layer_start_by_n_frames.jsx
@@ -0,0 +1,104 @@
+﻿// Arlo Emerson, 11/15/2019
+// arloemerson@gmail.com
+
+$.level = 2;
+
+// loop a comp and slide the start time of layers by N frames
+
+// important: select the comp, and then select the master layer that will be replacing everything found
+
+// see: https://buildmedia.readthedocs.org/media/pdf/after-effects-scripting-guide/latest/after-effects-scripting-guide.pdf
+
+// Added functionality to Array object as Adobe does implement modern javascript
+if(!Array.prototype.indexOf) 
+{
+    Array.prototype.indexOf = function(obj) 
+    {
+        for(var i=0; i<this.length; i++) 
+        {
+            if(obj===this[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    };
+}
+
+start();
+
+function start() {
+    app.beginUndoGroup("work_undo");
+
+    var layer1NameToFind = "asdf";
+    var compsChangedCounter = 0;
+    var frameTime = 0.033333; // 1 frame at 30 FPS
+
+    if (app.project != null) 
+    {
+        if (app.project.items.length < 1) {
+            alert("Please select at least one comp.");
+        } else {
+            var allComps = app.project.items;                                          
+            var selectedComps = 0;
+            collectionSelectedComps = new Array();
+
+            for (var i = allComps.length; i >= 1; i--) {
+                item = allComps[i];
+                if ((item instanceof CompItem) && item.selected) 
+                {
+                    selectedComps++;
+                    collectionSelectedComps[collectionSelectedComps.length] = item;
+                }
+            }
+
+            if (selectedComps >= 1) {
+                for (var k=0; k<collectionSelectedComps.length; k++) {                                
+                    var selectedComp = collectionSelectedComps[k];                    
+                    var ableToUpdate = false;
+                    
+                    //try to find the layer of interest by looping the comp's layer collection
+
+                    var arrayOfItems = [];
+
+                    for (var j=1; j<=selectedComp.layers.length; j++) { 
+                        // use this block if searching by name                    
+                        if (selectedComp.layers[j].name.indexOf( layer1NameToFind ) != -1){
+                            arrayOfItems.push( selectedComp.layers[j] );
+                        }
+                        // use this block if searching by type 
+                        // selectedComp.layers[j].matchName
+                        // if (selectedComp.layers[j].matchName == "ADBE Text Layer"){
+                        //     arrayOfItems.push( selectedComp.layers[j] );
+                        // }                     
+                    }
+
+                    for (var j=0; j<=arrayOfItems.length-1; j++){
+                        var tmpLayer = arrayOfItems[j];
+                        var tmpStartTime = arrayOfItems[j].startTime;
+                        tmpLayer.startTime = tmpStartTime + (frameTime*j); //1 frame at 30 FPS
+                    }
+                }
+
+            } else {
+                alert("Please select at least one comp.");
+            }
+        }        
+    } 
+
+    app.endUndoGroup();
+}
+
+
+function getComp(theName)
+{
+    for (var i = 1; i <= app.project.numItems; i++)
+    {
+        if ((app.project.item(i) instanceof CompItem) && (app.project.item(i).name == theName))
+        {
+            return app.project.item(i);
+        }
+    }
+    return null;
+}
+
---
+++ after_effects/__swap_in_new_comp.jsx
@@ -0,0 +1,123 @@
+﻿// Arlo Emerson, 11/27/2019
+// arloemerson@gmail.com
+
+// swap a layer with something from the project bin
+
+// Added functionality to Array object as Adobe does implement modern javascript
+if(!Array.prototype.indexOf) 
+{
+    Array.prototype.indexOf = function(obj) 
+    {
+        for(var i=0; i<this.length; i++) 
+        {
+            if(obj===this[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    };
+}
+
+start();
+
+
+function start() 
+{
+    var replacee = "asdfasdf";
+    var replacer = "asdfsadf_v2";
+    var layerToReplace = replacee;
+    var listOfReplacementComps = [replacer];
+
+    app.beginUndoGroup("dm_render");
+
+    statusReport = "";
+    counter = 0;
+
+    if (app.project != null) 
+    {
+        if (app.project.items.length < 1) 
+        {
+            alert("Please select at least one comp.");
+        } 
+        else 
+        {
+            var allComps = app.project.items;                                          
+            var selectedComps = 0;
+            collectionSelectedComps = new Array();
+
+            for (var i = allComps.length; i >= 1; i--) 
+            {
+                item = allComps[i];
+                if ((item instanceof CompItem) && item.selected) 
+                {
+                    selectedComps++;
+                    collectionSelectedComps[collectionSelectedComps.length] = item;
+                }
+            }
+
+            if (selectedComps >= 1)
+            {
+                for (var k=0; k<collectionSelectedComps.length; k++)
+                {
+                    for (var c=0; c<listOfReplacementComps.length; c++)
+                    {
+                        var replacementComp = getComp(listOfReplacementComps[c]);
+
+                        if (replacementComp == null)
+                        {
+                            alert ("Can't find comp " + listOfReplacementComps[c]);
+                            return;
+                        }
+
+                        var selectedComp = collectionSelectedComps[k];
+                        
+                        var ableToReplace = false;
+
+                        //try to find the layer by looping the comp's layer collection
+                        for (var j=1; j<=selectedComp.layers.length; j++)
+                        {
+                            if (selectedComp.layers[j].source != null) //layer must have a source
+                            {
+                                if (selectedComp.layers[j].source.name.indexOf( layerToReplace ) != -1 )
+                                {
+                                    selectedComp.layers[j].replaceSource(replacementComp,false);
+                                    selectedComp.layers[j].name = replacementComp.name;
+                                    ableToReplace = true;
+                                    selectedComp.label = 6;
+                                    counter++;
+                                }      
+                            }
+                        }
+
+                        if (ableToReplace == false)
+                        {
+                            alert("couldn't find a layer in " + selectedComp.name + " with name '" + layerToReplace + "'");
+                        }
+                    }
+                }
+            } 
+            else 
+            {
+                alert("Please select at least one comp.");
+            }
+        }        
+    } 
+
+    alert("made " + counter + " replacements. \n" + statusReport);
+    app.endUndoGroup();
+}
+
+
+function getComp(theName)
+{
+    for (var i = 1; i <= app.project.numItems; i++)
+    {
+        if ((app.project.item(i) instanceof CompItem) && (app.project.item(i).name == theName))
+        {
+            return app.project.item(i);
+        }
+    }
+    return null;
+}
+
