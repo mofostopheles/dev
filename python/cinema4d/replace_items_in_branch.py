835ae7dab1b10caf0a1fef31f9d3de388b86682e
Arlo Emerson <arloemerson@gmail.com>
2019-12-06 12:01:31 -0800

    Clean implementation of item replacement using the branch_iterator class

---
+++ python/cinema4d/replace_items_in_branch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+__author__ = "Arlo Emerson"
+__version__ = "1.0"
+__email__ = "arloemerson@gmail.com"
+
+'''
+	• Replace multiple instances of items within a particular branch of the tree.
+	• Deletes the replacee
+'''
+
+import c4d
+from c4d import documents as docs
+import branch_iterator as branch
+
+doc = docs.GetActiveDocument()
+first_item = doc.SearchObject("some_container")
+new_replacement = doc.SearchObject("asdf")
+tree = branch.BranchIterator(first_item)
+items_to_remove = []
+
+for item in tree:
+	if item.GetName() == "something_to_replace":
+		new_replacement.GetClone().InsertAfter( item )
+		items_to_remove.append(item)
+
+for remove_this in items_to_remove:
+	remove_this.Remove()
+
+c4d.EventAdd()
